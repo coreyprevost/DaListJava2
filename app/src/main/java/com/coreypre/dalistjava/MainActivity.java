c9ac62c6f156d343bb77667ba058080156fe6d50
“Corey <60476487+coreyprevost@users.noreply.github.com>
2020-08-12 20:16:35 -0500

    added DB-Firebase

--- app/src/main/java/com/coreypre/dalistjava/MainActivity.java
+++ app/src/main/java/com/coreypre/dalistjava/MainActivity.java
@@ -2,9 +2,12 @@ package com.coreypre.dalistjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -12,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.security.SecureRandom;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,8 +31,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+
+
         listView = findViewById(R.id.listView);
         button = findViewById(R.id.btn_AddToList);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -37,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         items = new ArrayList<>();
-        itemAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,items);
+        itemAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,items){
+            
+        };
         listView.setAdapter(itemAdapter);
         setUpListViewListener();
+
     }
 
     private void setUpListViewListener() {
@@ -67,5 +78,7 @@ public class MainActivity extends AppCompatActivity {
 
          }
 
+
     }
+
 }
